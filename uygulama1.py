a225e4e962b9ce2bd83c3116c79218b1c0d64f44
hmdllhscn <hmdllhscn@gmail.com>
2025-04-16 14:35:26 +0300

    uygulama1.py

--- uygulama1.py
+++ uygulama1.py
@@ -1,6 +1,6 @@
-not = int(input("Notunuzu girin (0-100): "))
+not1 = int(input("Notunuzu girin (0-100): "))
 
-if not < 0 or not_deg > 100:
+if not1 < 0 or not_deg > 100:
     print("Hatalı giriş")
 elif 90 <= not_deg <= 100:
     print("Pekiyi")
