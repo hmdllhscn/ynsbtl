3a75b602bc27b7b372dea75bcde2dbca48479ce5
hmdllhscn <hmdllhscn@gmail.com>
2025-04-16 14:32:27 +0300

    Add files via upload

---
+++ uygulama1.py
@@ -0,0 +1,12 @@
+not_deg = int(input("Notunuzu girin (0-100): "))
+
+if not_deg < 0 or not_deg > 100:
+    print("Hatalı giriş")
+elif 90 <= not_deg <= 100:
+    print("Pekiyi")
+elif 70 <= not_deg <= 89:
+    print("İyi")
+elif 50 <= not_deg <= 69:
+    print("Orta")
+else:
+    print("Zayıf")
